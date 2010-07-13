991fbf14e8adb39129bc2c6932564c22f8475199
Flávio Amieiro <flavio.amieiro@myfreecomm.com.br>
2010-07-13 19:12:27 -0300

    Added setup.py to install the plugin

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='ipdbplugin',
+    version='1.0',
+    url='http://github.com/flavioamieiro/nose-ipdb/tree/master',
+    author='Flavio Amieiro',
+    author_email = 'amieiro.flavio@gmail.com',
+    description = 'Nose plugin to use iPdb instead of Pdb',
+    long_description = \
+    """What about running nose with a smarter interactive debugger?
+
+    Use this and *never* risk yourself forgetting `import pdb; pdb.set_trace()` in your code again!
+
+    This plugin is 99.99% based on nose's builtin debug plugin.
+
+    If you have any ideas about how to improve it, come and fork the code at http://github.com/flavioamieiro/nose-ipdb
+    """,
+    install_requires=['nose', 'ipython', 'ipdb'],
+    license = 'GNU LGPL',
+    keywords = 'test unittest nose nosetests plugin debug ipdb ipython',
+    py_modules = ['ipdbplugin'],
+    entry_points = {
+        'nose.plugins.0.10': [
+            'ipdbplugin = ipdbplugin:iPdb'
+            ]
+        }
+    )
