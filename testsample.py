3975335b4b99cf89b84e1ea21607d02adca43697
Flavio Amieiro <amieiro.flavio@gmail.com>
2015-04-07 20:13:00 +0200

    Adds test case to trigger #16

--- testsample.py
+++ testsample.py
@@ -4,6 +4,12 @@ def test_error():
 def test_failure():
     assert 1 == 2, "This is a failure"
 
+def test_failure_with_print():
+    # This test is here to trigger issue #16
+    # https://github.com/flavioamieiro/nose-ipdb/issues/16
+    print("Test")
+    assert 1 == 2, "This failure has a print before it"
+
 def test_failure_with_local_variable():
     local_variable = 'foo'
     assert local_variable == 'bar', "This is a failure with a local variable in scope"
